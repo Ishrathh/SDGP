19a6cf98a1d91001852cedd8520cc8f262b25271
Ravindu2001 <ravindu.20200642@iit.ac.lk>
2023-04-05 09:17:05 +0530

    FrontEnd

--- server-backend/routes/userRouter.js
+++ server-backend/routes/userRouter.js
@@ -1,3 +1,12 @@
+import {Router} from 'express';
+import { register } from '../controllers/user.js';
+
+
+const userRouter = Router();
+userRouter.post {'/register', register} ;
+userRouter.post{'/login', login} ;
+
+
 const register = require('../controllers/user.js');
 const router = require('express').Router();
 
