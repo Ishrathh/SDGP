ef83d8f7d1b1648d55593c4764a28f740ade8bb5
Ishrathh <ishrath.20210696@iit.ac.lk>
2023-04-06 03:00:25 +0530

    Added python script

--- server-backend/routes/model_loader.py
+++ server-backend/routes/model_loader.py
@@ -4,16 +4,19 @@ import json
 import pickle
 
 #Loading the model
-with open('addiction_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# with open('C:/Users/Phanthom/MEGAsync/IIT/SEM 2/SDGP/GitHub/server-backend/routes/addiction_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
+
+print("Just testing to see if this shit works ffs!")
 # Get the input array from the command-line arguments
-input_str = sys.argv[1]
-inputs = json.loads(input_str)
+input_array = sys.argv[1]
+# inputs = json.loads(input_array)
+# inputs = [[float(x) for x in inputs[0]]]
+print('Received input data:', input_array)
 
-print(inputs)
-
-output = model.predict(inputs)
+# print(inputs)
+# output = model.predict(input_array)
 
 # Print the prediction
-print(output[0])
+# print(output)
