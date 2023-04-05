1fcaff6562232acd8f30ba1b36c078937d199e99
Neluka Fernando <neluka.20200331@iit.ac.lk>
2023-04-05 17:13:56 +0530

    Login and register feature completed

--- dashboard-frontend/src/pages/LoginPage.jsx
+++ dashboard-frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import '../css/LoginPage.css';
 import axios from 'axios';
-//import { login } from '../actions/user';
-
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ });
 
   const loginUser = async () => {
     const credentials = formData;
@@ -12,13 +14,15 @@ function LoginPage() {
     //Saving user to the database
     await axios.post("http://localhost:8000/user/login", credentials)
       .then((response) => {
-           
+           console.log(response.data);
+
         //Saving user data to local storage
-        //localStorage.setItem("userName", response.data.userName);
-        //localStorage.setItem("userLevel", response.data.userLevel);
+        localStorage.setItem("name", response.data.name);
+        localStorage.setItem("addictionLevel", response.data.addictionLevel);
+        localStorage.setItem("email", response.data.email);
       })
       .catch((error) => {
-        //alert(error.response.data.message);
+        alert("Invalid email or password");
         console.log(error);
       });
   }
@@ -26,10 +30,9 @@ function LoginPage() {
   const handleSubmit = (e)=> {
     e.preventDefault();
     loginUser();
-    //return login( formData);
+    navigate('/dashboard');
   }
 
-  const [formData, setFormData] = useState({ });
   const handleChange = (id, value) => {
     setFormData({
       ...formData, 
@@ -53,7 +56,7 @@ function LoginPage() {
           <div className="line"></div>
           <a href="/signup">Don't have an account? Sign up</a>
           <br />
-          <input type="submit" value="Sign In" class="login-btn" /><br />
+          <input type="submit" value="Sign In" className="login-btn" /><br />
         </form>
       </div>
     </div>
--- dashboard-frontend/src/pages/SignupPage.jsx
+++ dashboard-frontend/src/pages/SignupPage.jsx
@@ -1,49 +1,68 @@
-import React, {useRef} from 'react';
+import React, { useState} from 'react';
 import '../css/SignupPage.css';
-
-
-const handleSubmit = (e)=> {
-  //e.preventDefault();
-  //const email= emailRef.current.value
-  //const password = passwordRef.current.value
-  //const name = nameRef.current.value
-  //const confirmPassword = confirmPassword.current.value
-  //if (password !== confirmPassword) 
-  //return dispatch({
-  //  type:'UPDATE_ALERT', 
-  //  payload: {
-  //    open:true, 
-   //     severity:'error', 
-//message:'Passwords do not match',
-   //   },
-   // });
-  //  register({name, email, password}, dispatch);
-  }
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
   
-  function SignupPage() {
-  //const nameRef = useRef();
-  //const emailRef = useRef();
-  //const passwordRef = useRef();
-  //const confirmPasswordRef = useRef();
+function SignupPage() {
+
+  const navigate = useNavigate();
+  const [ formData, setFormData ] = useState({});
+
+  const handleChange = (id, value) => {
+    setFormData({
+      ...formData,
+      [id]: value
+    })
+  }
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+
+    if ( formData.password.length < 6) {
+      alert('Password must be at least 6 characters');
+      return;
+    } else if ( formData.password !== formData.repassword) { 
+      alert('Passwords do not match');
+      return;
+    } else {
+      handleRegister();
+      navigate('/');
+    }
+  }
+
+  const handleRegister = async () => {
+    const userDetails = formData;
+
+    //Saving new user to the database
+    await axios.post("http://localhost:8000/user/register", userDetails) 
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+            alert('Error occured when registering user');
+        })
+  }
+
   return (
     <div className="container">
       <div className="signup-box">
         <h2>Sign up</h2>
         <form onSubmit={handleSubmit}>
           <div className="user-box">
-            <input type="text" name="name" required />
+            <input type="text" name="name" required id='name' onChange={(e) => handleChange(e.target.id, e.target.value)}/>
             <label>Name</label>
           </div>
           <div className="user-box">
-            <input type="email" name="email" required />
+            <input type="email" name="email" required id='email'  onChange={(e) => handleChange(e.target.id, e.target.value)}/>
             <label>Email</label>
           </div>
           <div className="user-box">
-            <input type="password" name="password" required />
+            <input type="password" name="password" required id='password' onChange={(e) => handleChange(e.target.id, e.target.value)}/>
             <label>Password</label>
           </div>
           <div className="user-box">
-            <input type="password" name="confirm-password" required />
+            <input type="password" name="confirm-password" id='repassword' required onChange={(e) => handleChange(e.target.id, e.target.value)}/>
             <label>Confirm Password</label>
           </div>
           <input type="submit" value="Sign up" />
