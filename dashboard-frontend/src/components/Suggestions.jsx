a6304f26e51944de028235d849b0d9b3def8d598
Ishrathh <ishrath.20210696@iit.ac.lk>
2023-04-06 09:37:00 +0530

    Modified Server
    Added API for recommendation model

--- dashboard-frontend/src/components/Suggestions.jsx
+++ dashboard-frontend/src/components/Suggestions.jsx
@@ -1,12 +1,86 @@
-import React from 'react'
-import '../css/Suggestions.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../css/Suggestions.css';
 
 const Suggestions = () => {
+  const [suggestion, setSuggestion] = useState("Click on 'Get Hobby'");
+
+  const changeSuggestion = (newSuggestion) => {
+    setSuggestion('');
+    setSuggestion(newSuggestion);
+  }
+
+  const handleGetHobby = async () => {
+    await axios
+      .post('http://localhost:8000/getHobby')
+      .then((response) => {
+        console.log(response.data);
+        changeSuggestion(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Error occured');
+      });
+  };
+
+  const handleLike = async () => {
+    await axios
+      .post('http://localhost:8000/like', suggestion.toString)
+      .then((response) => {
+        console.log(response.data);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Error occured');
+      });
+  };
+
+  const handleDislike = async () => {
+    await axios
+      .post('http://localhost:8000/dislike')
+      .then((response) => {
+        console.log(response.data);
+        alert('Addiction Level: ' + response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Error occured');
+      });
+  };
+
+  const handleRecommendedHobby = async () => {
+    await axios
+      .post('http://localhost:8000/recommendedHobby')
+      .then((response) => {
+        console.log(response.data);
+        alert('Addiction Level: ' + response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Error occured');
+      });
+  };
+
   return (
     <>
-      <div className='suggestion'>Take a walk for a change.</div>
+      <div className='suggestion'>{suggestion}</div>
+      <div className='btnPane'>
+        <button type='submit' className='btn btn-primary darkBtn' onClick={handleGetHobby}>
+          Get Hobby
+        </button>
+        <button type='submit' className='btn btn-primary darkBtn' onClick={handleLike}>
+          Like 👍🏽
+        </button>
+        <button type='submit' className='btn btn-primary darkBtn' onClick={handleDislike}>
+          Dislike 👎🏽
+        </button>
+        <button type='submit' className='btn btn-primary darkBtn' onClick={handleRecommendedHobby}>
+          Recommended Hobby
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Suggestions
+export default Suggestions;
